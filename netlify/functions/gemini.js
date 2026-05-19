d8fd0d1aa05a51d6d9c550b32d8ec942cce386f7
kabeersabharwal032-commits <kabeersabharwal032@gmail.com>
2026-05-19 17:17:31 +0530

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -1,1 +1,45 @@
+exports.handler = async function(event) {
+  try {
+    const body = JSON.parse(event.body);
+    const prompt = body.prompt;
 
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        reply:
+          data.candidates?.[0]?.content?.parts?.[0]?.text ||
+          "No response"
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
